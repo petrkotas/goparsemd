3da55967b674b54454569df8442bbd062fcb145b
Petr Kotas <petr.kotas@gmail.com>
2019-10-10 18:32:06 +0200

    Add file read
    Signed-off-by: Petr Kotas <petr.kotas@gmail.com>

--- parse.go
+++ parse.go
@@ -2,13 +2,37 @@ package main
 
 import (
 	"flag"
+	"io/ioutil"
 	"log"
+	"os"
 )
 
 var dataPath string
 
+// also can use ioutil.ReadFile(filename)
+func readFile(file string) ([]byte, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	return ioutil.ReadAll(f)
+}
+
+// main function is the entrypoint of an application
 func main() {
 	log.Printf("Data path %s", dataPath)
+
+	files, err := ioutil.ReadDir(dataPath)
+	if err != nil {
+		log.Fatalf("Cannot load data, %s", err.Error())
+	}
+
+	for _, file := range files {
+		log.Printf("File: %s", file.Name())
+	}
+
 }
 
 // init function is executed first,
